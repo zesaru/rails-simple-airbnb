1e40be83387ee5f7506fb64644e9c6a0d97ab836
Cesar Murillo <namiayumis@gmail.com>
2019-05-22 17:33:31 +0900

    air

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+  },
+  {
+  name: 'Spacious Flat Tokyo',
+  address: '10 Clifton Meguro',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 5
+  },
+  {
+  name: 'Romantic, Lakeside Home with Views of Lake Como',
+  address: 'Kanagawa City',
+  description: 'Wake up to stunning views of Lake Como from every window of this romantic',
+  price_per_night: 80,
+  number_of_guests: 2
+  },
+  {
+  name: 'La Salentina, sea, nature & relax',
+  address: 'Enoshima City',
+  description: 'A lovely place to experience the Solento Coast',
+  price_per_night: 40,
+  number_of_guests: 9
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
